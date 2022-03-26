a7faf9c2af322a3267a68a97318b864e37453db9
MRprgmr <algoprogram01@gmail.com>
2022-03-26 14:58:22 +0500

    Changes in updater

--- handlers/updater.py
+++ handlers/updater.py
@@ -1,19 +1,18 @@
 import asyncio
 
-from aiogram import types, Dispatcher, Bot
+from aiogram import types, Bot
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from loader import dp
 
 loop = asyncio.get_event_loop()
-Dispatcher.set_current(dp)
-Bot.set_current(dp.bot)
 
 
 class UpdateBot(APIView):
     def post(self, request):
         try:
+            Bot.set_current(dp.bot)
             update = types.Update(**request.data)
             loop.run_until_complete(dp.process_update(update))
         except Exception as e:
